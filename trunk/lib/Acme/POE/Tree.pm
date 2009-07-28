657110a4a250f05a98e1522b3784ad17cb4f346a
Rocco Caputo <rcaputo@cpan.org>
2009-07-28 05:58:11 -0400

    Rely on latest POE.  Prepare for a new distribution.

--- trunk/lib/Acme/POE/Tree.pm
+++ trunk/lib/Acme/POE/Tree.pm
@@ -11,7 +11,7 @@ use constant CYCLE_TYPE => "random"; # "random" or "cycle"
 use constant LIGHT_TYPE => "strand"; # "random" or "strand"
 use constant DIM_BULBS => 0; # enable dim bulbs
 
-our $VERSION = '1.01';
+our $VERSION = '1.020';
 
 sub new {
 	my ($class, $arg) = @_;
